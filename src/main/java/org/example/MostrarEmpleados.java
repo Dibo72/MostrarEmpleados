956a4aa8151b694b9a0d85a9b3e68100ec0e543d
Dibo72 <humanida133@gmail.com>
2026-03-24 11:51:34 +0100

    Ejercicio acabado

--- src/main/java/org/example/MostrarEmpleados.java
+++ src/main/java/org/example/MostrarEmpleados.java
@@ -1,17 +1,29 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class MostrarEmpleados {
+    public static void main(String[] args) {
+        try(Connection conn = DriverManager.getConnection(
+                DBConfig.getUrl(),
+                DBConfig.getUser(),
+                DBConfig.getPassword());
+            Statement stmt =conn.createStatement()){
+
+            String sql = "SELECT empleado.nombre as empleado, departamento.nombre as departamento " +
+                            "FROM empleado JOIN departamento ON departamento_id = departamento.id";
+            ResultSet rs = stmt.executeQuery(sql);
+            while(rs.next()){
+                String empleado = rs.getString("empleado");
+                String departamento = rs.getString("departamento");
+                System.out.println(empleado + " " + departamento);
+            }
+
+        }catch(Exception e){
+            System.out.println(e.getMessage());
         }
     }
 }
